809890377dae1769e4e995dce1fbb5324450fddb
davidnga <87443465+davidnga@users.noreply.github.com>
2022-10-05 20:32:22 -0400

    1.0

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('scroll', function (e) {
+    $(".imgs").removeClass("opacity-0");
+    $(".imgs").removeClass("opacity-0");
+    $("#ler").addClass("animate");
+    $("#fut").addClass("animate-2");
+    $("#ler").addClass("opacity");
+    $("#fut").addClass("opacity");
+
+    console.log("aa")
+})
